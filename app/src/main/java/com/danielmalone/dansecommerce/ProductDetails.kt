b16b764ea183b9bd8c1c794eded664aa8cc49a80
Daniel Malone <danieljmalone@gmail.com>
2018-08-06 18:29:19 -0500

    Finish YouTube video part 3

---
+++ app/src/main/java/com/danielmalone/dansecommerce/ProductDetails.kt
@@ -0,0 +1,17 @@
+package com.danielmalone.dansecommerce
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.product_details.*
+
+class ProductDetails : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.product_details)
+
+        val title = intent.getStringExtra("title")
+        product_name.text = title
+
+    }
+}
--- app/src/main/java/com/danielmalone/dansecommerce/ProductsAdapter.kt
+++ app/src/main/java/com/danielmalone/dansecommerce/ProductsAdapter.kt
@@ -1,5 +1,6 @@
 package com.danielmalone.dansecommerce
 
+import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -20,7 +21,13 @@ class ProductsAdapter(private val products: ArrayList<Product>) : RecyclerView.A
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.product_row, parent, false)
-        return ViewHolder(view)
+        val holder = ViewHolder(view)
+        view.setOnClickListener {
+            val intent = Intent(parent.context, ProductDetails::class.java)
+            intent.putExtra("title", products[holder.adapterPosition].title)
+            parent.context.startActivity(intent)
+        }
+        return holder
     }
 
     override fun getItemCount() = products.size
