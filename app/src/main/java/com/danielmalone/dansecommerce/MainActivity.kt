0750fa42b1f2789d3daa988dba3ff45d60c7b152
Daniel Malone <danieljmalone@gmail.com>
2018-08-11 13:28:27 -0500

    Finish video #6

--- app/src/main/java/com/danielmalone/dansecommerce/MainActivity.kt
+++ app/src/main/java/com/danielmalone/dansecommerce/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.danielmalone.dansecommerce
 
 import android.os.Bundle
+import android.support.v4.view.GravityCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.GridLayoutManager
+import android.view.MenuItem
 import com.danielmalone.dansecommerce.model.Product
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 import kotlinx.android.synthetic.main.main.*
 
@@ -13,6 +16,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        navigationView.setNavigationItemSelectedListener {
+            it.isChecked = true
+            drawerLayout.closeDrawers()
+            true
+        }
+
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp)
+        }
 
         val products = arrayListOf<Product>()
 
@@ -24,5 +38,11 @@ class MainActivity : AppCompatActivity() {
             layoutManager = GridLayoutManager(this@MainActivity, 2)
             adapter = ProductsAdapter(products)
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        drawerLayout.openDrawer(GravityCompat.START)
+        return true
+//        return super.onOptionsItemSelected(item)
     }
 }
