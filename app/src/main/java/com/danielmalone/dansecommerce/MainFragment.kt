5c6c7f9576914b915cfe2f17ba1911e498e5859b
Daniel Malone <danieljmalone@gmail.com>
2019-04-20 14:27:32 -0500

    finish video #16

--- app/src/main/java/com/danielmalone/dansecommerce/MainFragment.kt
+++ app/src/main/java/com/danielmalone/dansecommerce/MainFragment.kt
@@ -1,31 +1,56 @@
 package com.danielmalone.dansecommerce
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
+import com.danielmalone.dansecommerce.database.AppDatabase
 import com.danielmalone.dansecommerce.model.Product
-import com.google.gson.Gson
 import kotlinx.android.synthetic.main.fragment_main.view.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
-import java.net.URL
 
 class MainFragment : androidx.fragment.app.Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val root = inflater.inflate(R.layout.fragment_main, container, false)
 
+//        doAsync {
+//            val json = URL("https://finepointmobile.com/data/products.json").readText()
+//
+//            uiThread {
+//                val products = Gson().fromJson(json, Array<Product>::class.java).toList()
+//
+//                root.recycler_view.apply {
+//                    layoutManager = androidx.recyclerview.widget.GridLayoutManager(activity, 2)
+//                    adapter = ProductsAdapter(products)
+//                    root.progressBar.visibility = View.GONE
+//                }
+//            }
+//        }
+
+
         doAsync {
-            val json = URL("https://finepointmobile.com/data/products.json").readText()
+
+            val db = Room.databaseBuilder(
+                    activity!!.applicationContext,
+                    AppDatabase::class.java, "database-name"
+            ).build()
+
+            val productsFromDatabase = db.productDao().getAll()
+
+            val products = productsFromDatabase.map {
+                Product(
+                        it.title,
+                        "https://finepointmobile.com/data/jeans2.jpg",
+                        it.price,
+                        true
+                )
+            }
 
             uiThread {
-                val products = Gson().fromJson(json, Array<Product>::class.java).toList()
-
                 root.recycler_view.apply {
                     layoutManager = androidx.recyclerview.widget.GridLayoutManager(activity, 2)
                     adapter = ProductsAdapter(products)
@@ -33,6 +58,8 @@ class MainFragment : androidx.fragment.app.Fragment() {
                 }
             }
         }
+
+//        root.progressBar.visibility = View.GONE
 
         val categories = listOf("Jeans", "Socks", "Suits", "Skirts", "Dresses", "Jeans", "Socks", "Pants", "Jackets", "Daniel")
 
