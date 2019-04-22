612149fb6332ef76b70e6e432ac95ed302bd5d9d
Daniel Malone <danieljmalone@gmail.com>
2019-04-22 18:24:03 -0500

    finish video #20

--- app/src/main/java/com/danielmalone/dansecommerce/MainFragment.kt
+++ app/src/main/java/com/danielmalone/dansecommerce/MainFragment.kt
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.room.Room
 import com.danielmalone.dansecommerce.database.AppDatabase
 import com.danielmalone.dansecommerce.model.Product
+import kotlinx.android.synthetic.main.fragment_main.*
 import kotlinx.android.synthetic.main.fragment_main.view.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
@@ -17,50 +18,6 @@ class MainFragment : androidx.fragment.app.Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val root = inflater.inflate(R.layout.fragment_main, container, false)
 
-//        doAsync {
-//            val json = URL("https://finepointmobile.com/data/products.json").readText()
-//
-//            uiThread {
-//                val products = Gson().fromJson(json, Array<Product>::class.java).toList()
-//
-//                root.recycler_view.apply {
-//                    layoutManager = androidx.recyclerview.widget.GridLayoutManager(activity, 2)
-//                    adapter = ProductsAdapter(products)
-//                    root.progressBar.visibility = View.GONE
-//                }
-//            }
-//        }
-
-
-        doAsync {
-
-            val db = Room.databaseBuilder(
-                    activity!!.applicationContext,
-                    AppDatabase::class.java, "database-name"
-            ).build()
-
-            val productsFromDatabase = db.productDao().getAll()
-
-            val products = productsFromDatabase.map {
-                Product(
-                        it.title,
-                        "https://finepointmobile.com/data/jeans2.jpg",
-                        it.price,
-                        true
-                )
-            }
-
-            uiThread {
-                root.recycler_view.apply {
-                    layoutManager = androidx.recyclerview.widget.GridLayoutManager(activity, 2)
-                    adapter = ProductsAdapter(products)
-                    root.progressBar.visibility = View.GONE
-                }
-            }
-        }
-
-//        root.progressBar.visibility = View.GONE
-
         val categories = listOf("Jeans", "Socks", "Suits", "Skirts", "Dresses", "Jeans", "Socks", "Pants", "Jackets", "Daniel")
 
         root.categoriesRecyclerView.apply {
@@ -69,5 +26,39 @@ class MainFragment : androidx.fragment.app.Fragment() {
         }
 
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        searchButton.setOnClickListener {
+
+            doAsync {
+
+                val db = Room.databaseBuilder(
+                        activity!!.applicationContext,
+                        AppDatabase::class.java, "database-name"
+                ).build()
+
+                val productsFromDatabase = db.productDao().searchFor("%${searchTerm.text}%")
+
+                val products = productsFromDatabase.map {
+                    Product(
+                            it.title,
+                            "https://finepointmobile.com/data/jeans2.jpg",
+                            it.price,
+                            true
+                    )
+                }
+
+                uiThread {
+                    recycler_view.apply {
+                        layoutManager = androidx.recyclerview.widget.GridLayoutManager(activity, 2)
+                        adapter = ProductsAdapter(products)
+                    }
+                    progressBar.visibility = View.GONE
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/danielmalone/dansecommerce/database/ProductDao.kt
+++ app/src/main/java/com/danielmalone/dansecommerce/database/ProductDao.kt
@@ -10,6 +10,9 @@ interface ProductDao {
     @Query("SELECT * FROM ProductFromDatabase")
     fun getAll(): List<ProductFromDatabase>
 
+    @Query("SELECT * FROM ProductFromDatabase WHERE title LIKE :term")
+    fun searchFor(term: String): List<ProductFromDatabase>
+
     @Insert
     fun insertAll(vararg products: ProductFromDatabase)
 }
