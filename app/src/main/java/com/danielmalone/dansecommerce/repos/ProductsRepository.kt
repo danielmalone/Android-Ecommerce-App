530c3b7174620736015d44c1e0e3d77e987f873e
Daniel Malone <danieljmalone@gmail.com>
2019-05-11 10:43:32 -0500

    finish video #26

--- app/src/main/java/com/danielmalone/dansecommerce/repos/ProductsRepository.kt
+++ app/src/main/java/com/danielmalone/dansecommerce/repos/ProductsRepository.kt
@@ -9,7 +9,7 @@ class ProductsRepository {
 
     fun getAllProducts(): Single<List<Product>> {
         return Single.create<List<Product>> {
-            val json = URL("https://finepointmobile.com/data/products.json").readText()
+            val json = URL("https://gist.githubusercontent.com/danielmalone/df34a33a06e985d85f2ba7f6e635c600/raw/df0aeaea2def4aa1c9b0f266d032b6733c3fbaaa/shopping_products.json").readText()
             val products = Gson().fromJson(json, Array<Product>::class.java).toList()
             it.onSuccess(products)
         }
